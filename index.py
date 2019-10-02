ce6e17f6cec92e27dacd3d495aff8a7e25de570c
Diego Algara <diego.algara@gmail.com>
2019-10-02 00:25:20 -0500

    wip rules of queens

--- index.py
+++ index.py
@@ -1,34 +1,48 @@
 #!/usr/bin/env python3
-import sys
 
 n = 4 # Board Size
 q = [] # Queen Solutions
-t = [] # Temp
-
-
-# print("Usage: python3 index.py [N]")
-
-# if len(sys.argv) != 2:
-# 	print("Size unspecified")
-# elif str.isdigit(sys.argv[1]): # includes 0
-# 	n = int(sys.argv[1])
-# else:
-# 	print("N should be a positive integer")
+b = [[0] * n for _ in range(n)] # Temp board
 
 print("Solving for", n)
 
-b = [0]*n*n
+def pBoard(b):
+	print()
+	for x in range(n):
+		print('{:>2}'.format(n-x), end = '| ')
+		for y in range(n):
+			print(b[x][y], end = ' ')
+			if y == (n-1):
+				print()
+	return True
 
-q = b
+def isFree(b,x,y):
+	# same row
+	for i in range(x):
+		if b[i][y] == 1:
+			return False
 
-print()
+	# same column
+	for i in range(y):
+		if b[x][i] == 1:
+			return False
+
+	# up left diagonal
+	for i,j in zip(range(x, -1, -1), range(y, -1, -1)): 
+		if b[i][j] == 1: 
+			return False
+
+	# up right diagonal
+	for i,j in zip(range(x, -1, -1), range(y, n)): 
+		if b[i][j] == 1: 
+			return False
+
+	return True
+
 for x in range(n):
 	for y in range(n):
-		print(b[x*n+y], end = ' ')
-		if y == (n-1):
-			print()
-		# if q[x][y] == 0 and b[x][y] == 0:
-		# 	q[x][y] = 1
-		# 	b[x]
+		if isFree(b,x,y):
+			b[x][y] = 1
+			pBoard(b)
+			# continue
 
-	
