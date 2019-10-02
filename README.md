5213406fdfef75f0d5dc70d467248a44c16cf2cc
Diego Algara <diego.algara@gmail.com>
2019-10-02 16:44:25 -0500

    readme, wip

---
+++ README.md
@@ -0,0 +1,14 @@
+# N Queens problem solver in Python 3
+
+## Usage:
+```
+python3 solver.py [max_size] [min_size] [log_level]
+ max_size  : Board maximum size, default = 14
+ min_size  : Board initial size, default = 1
+ log_level : Output detail shown in the process, default = 0
+             Recommended only whith min_size = max_size
+             0 Only show statistics
+             1 Dispaly all solutions with boards
+             2 Dispaly all steps in the process, queen boards
+             3 Dispaly all steps in the process, attack maps
+```
--- solver.py
+++ solver.py
@@ -3,6 +3,28 @@ import time
 import copy
 import sys
 
+print("Usage: python3 solver.py [max_size] [min_size] [log_level]")
+print("max_size  : Board maximum size, default = 14")
+print("min_size  : Board initial size, default = 1")
+print("log_level : Output detail shown in the process, default = 0")
+print("            Recommended only whith min_size = max_size")
+print("            0 Only show statistics")
+print("            1 Dispaly all solutions with boards")
+print("            2 Dispaly all steps in the process, queen boards")
+print("            3 Dispaly all steps in the process, attack maps")
+print()
+min = 1
+max = 15
+show = False
+debug = False
+
+if len(sys.argv) != 2:
+	print("Size unspecified")
+elif str.isdigit(sys.argv[1]):
+	n = int(sys.argv[1])
+else:
+	print("N should be a positive integer")
+
 def pBoard(b):
 	print()
 	for x in range(n):
@@ -55,9 +77,9 @@ def solve(b,x,n):
 			b[x][y] = 0
 
 
-for n in range(13):
+for n in range(min, max):
 	s = [] # Solutions
-	b = [[0] * n for _ in range(n)]
+	b = [[0] * n for _ in range(n)] # Board
 	start = time.time()
 	print("N =", n, end = '| ')
 	solve(b, 0, n)
